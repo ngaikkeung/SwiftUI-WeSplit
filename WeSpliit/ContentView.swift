ca098dd4014c6ab1b111d12648dca01f1968ed5d
ngaikkeung <ngaikkeung@gmail.com>
2022-04-02 22:29:20 +0800

    Initial Commit

---
+++ WeSpliit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSpliit
+//
+//  Created by KK NGAI on 2/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSpliit/WeSpliitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSpliitApp.swift
+//  WeSpliit
+//
+//  Created by KK NGAI on 2/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSpliitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
